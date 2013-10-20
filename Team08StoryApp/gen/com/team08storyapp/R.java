34bf5c7648f64a331de855190826537ff25eeec0
toll89 <toll@ualberta.ca>
2013-10-20 12:37:48 -0600

    Added StoryListActivity. Added Id's to buttons of MainActivity.

--- Team08StoryApp/gen/com/team08storyapp/R.java
+++ Team08StoryApp/gen/com/team08storyapp/R.java
@@ -24,13 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int my_stories_button=0x7f080002;
+        public static final int read_download_button=0x7f080001;
+        public static final int read_online_button=0x7f080000;
+        public static final int search=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_story_list=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int story_list=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -39,7 +45,9 @@ public final class R {
         public static final int my_stories=0x7f050006;
         public static final int read_download=0x7f050005;
         public static final int read_online=0x7f050004;
+        public static final int search_hint=0x7f050008;
         public static final int story_welcome=0x7f050003;
+        public static final int title_activity_story_list=0x7f050007;
     }
     public static final class style {
         /** 
