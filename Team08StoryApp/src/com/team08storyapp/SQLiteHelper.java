95afd2950e621b53eeaf8bb1771caa919cdce1f3
UliceNix <mingxun@ualberta.ca>
2013-10-19 20:51:14 -0600

    Create SQLiteHelper.java

---
+++ Team08StoryApp/src/com/team08storyapp/SQLiteHelper.java
@@ -0,0 +1,86 @@
+package com.team08storyapp;
+
+import java.util.ArrayList;
+
+public class SQLiteHelper {
+
+	public boolean addStory(Story s1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addStoryFragment(StoryFragment sf1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addAnnotationPhoto(Photo p2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addChoice(Choice c2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addStoryPhoto(Photo p1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateStory(Story s1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateStoryFragment(StoryFragment sf1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Story getStory(int SId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Story> getStories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<StoryFragment> getStoryFragments(int SId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Photo> getStoryPhotos(int FId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Photo> getAnnotationPhotos(int FId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Choice> getChoices(int FId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public StoryFragment getStoryFragment(int storyFragmentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Story> searchForStory(String searchText) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+}
