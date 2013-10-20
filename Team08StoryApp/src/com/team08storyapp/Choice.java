33490444b334be972a600432a77215122daa342e
UliceNix <mingxun@ualberta.ca>
2013-10-19 20:41:14 -0600

    Update Choice.java

--- Team08StoryApp/src/com/team08storyapp/Choice.java
+++ Team08StoryApp/src/com/team08storyapp/Choice.java
@@ -1,5 +1,29 @@
 package com.team08storyapp;
 
 public class Choice {
+	
+	String text;
+	int curFragmentID;
+	int FragmentID;
+	int Id;
+	
+	public Choice(int curFragmentID){
+		this.curFragmentID = curFragmentID;
+		
+	}
+	
+	public void setText(String string) {
+		// TODO Auto-generated method stub
+		this.text = string;
+	}
+	
+	public void setId(int CId){
+		this.Id = CId;
+	}
+	
+	public void setFragmentID(int FId){
+		this.FragmentID = FId;
+		
+	}
 
 }
