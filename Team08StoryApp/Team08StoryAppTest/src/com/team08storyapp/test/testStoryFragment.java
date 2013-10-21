8b06f215e6ef708da66c43bf97326f1a30d16878
Jiawei Shen <jishent@gmail.com>
2013-10-20 23:51:16 -0600

    Added new StoryFrag

--- Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragment.java
+++ Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragment.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import junit.framework.TestCase;
 
-public class testStory extends TestCase {
+public class testStoryFragment extends TestCase{
 
 	@Test
 	public void test() {
