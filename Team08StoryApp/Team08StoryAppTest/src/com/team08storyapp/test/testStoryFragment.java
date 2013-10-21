36cc4f40ddfe396f3f5ae43f1c881a792d7a2ea9
Jiawei Shen <jishent@gmail.com>
2013-10-21 00:13:25 -0600

    Updates testStory

--- Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragment.java
+++ Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragment.java
@@ -4,8 +4,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import junit.framework.TestCase;
 
+import com.team08storyapp.Choice;
+import com.team08storyapp.Photo;
+import com.team08storyapp.SQLiteHelper;
+import com.team08storyapp.Story;
+import com.team08storyapp.StoryController;
+import com.team08storyapp.StoryFragment;
+
 public class testStoryFragment extends TestCase{
 
+	
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
