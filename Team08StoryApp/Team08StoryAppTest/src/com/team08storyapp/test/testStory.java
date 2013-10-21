17948401c8d7bafe4b1d506a9b3ea5ac1ca86b99
Jiawei Shen <jishent@gmail.com>
2013-10-20 23:49:08 -0600

    test

---
+++ Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStory.java
@@ -0,0 +1,14 @@
+package com.team08storyapp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testStory {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
