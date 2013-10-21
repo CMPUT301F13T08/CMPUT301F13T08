3198df4af3fa2b50e452758202e5eeb072658db1
Jiawei Shen <jishent@gmail.com>
2013-10-21 00:25:04 -0600

    Updated testStoryFragment

--- Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragment.java
+++ Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragment.java
@@ -1,6 +1,9 @@
 package com.team08storyapp.test;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 import junit.framework.TestCase;
 
@@ -10,12 +13,26 @@ import com.team08storyapp.SQLiteHelper;
 import com.team08storyapp.Story;
 import com.team08storyapp.StoryController;
 import com.team08storyapp.StoryFragment;
+import com.team08storyapp.Annotation;
 
 public class testStoryFragment extends TestCase{
 
+	private String storyAuthor;
+	private ArrayList<java.lang.annotation.Annotation> fragAnnotations;
+	private ArrayList<Choice> fragChoices;
+	private ArrayList<Photo> fragPhotos;
+	private int fragmentID;
+	
+	protected void setUp(){
+		fragmentID = 1;
+		storyAuthor = "Authour";
+		fragAnnotations = new ArrayList<Annotation>();
+		fragChoices = new ArrayList<Choice>();
+		fragPhoto = new ArrayList<Photo>();
+		
+	}
 	
 	
-	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
