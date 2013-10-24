58ad8a97f167924a36273e191a6996ca871dd3ba
Jiawei Shen <Jiawei@Shen-Jiaweis-MacBook-Pro-2.local>
2013-10-24 01:49:53 -0600

    Up

--- Team08StoryApp/src/com/team08storyapp/StoryFragmentController.java
+++ Team08StoryApp/src/com/team08storyapp/StoryFragmentController.java
@@ -5,8 +5,19 @@ import java.util.ArrayList;
 public class StoryFragmentController {
 	
 	public void addAnnotation(Annotation annotation){
-		
-		
+		// TODO Auto-generated method stub
+	}
+	
+	public void addChoice(Choice choice){
+		// TODO Auto-generated method stub
+	}
+	
+	public void addImage (Photo photo){
+		// TODO Auto-generated method stub
+	}
+	
+	public void updateStoryfragment(StoryFragment storyfragment){
+		// TODO Auto-generated method stub
 	}
 
 }
