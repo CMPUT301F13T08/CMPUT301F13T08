d0232151ea231401f9a9c892f2f5a0b99ebd9cdc
marcugit <marcu@ualberta.ca>
2013-10-24 16:49:02 -0600

    Update testPhotoController.java

--- Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testPhotoController.java
+++ Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testPhotoController.java
@@ -4,11 +4,12 @@ import android.provider.ContactsContract.CommonDataKinds.Photo;
 
 import com.team08storyapp.Story;
 
-
+// test cases for methods of the PhotoController class
 public class testPhotoController extends TestCase
 {
 
-	
+// test the method TakePhoto. If a photo is successfully taken, then the picture parameter
+// of the photo object should contain the picture (not be null)
 public void testTakePhoto(){
 		
 	
@@ -17,7 +18,9 @@ public void testTakePhoto(){
 		assertNotNull(photo.picture);
 
 	}
-	
+
+// uploading a photo into a photo object should set the picture parameter to the uploaded image.
+// the picture should not be null
 public void testUploadPhoto(){
 	
 	
