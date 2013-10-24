389316a6e761332bd171e0578eeac640825e0dc6
Jiawei Shen <Jiawei@uws27642usr.uws.ualberta.ca>
2013-10-23 20:31:02 -0600

    Update testStoryFragment

--- Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragmentController.java
+++ Team08StoryApp/Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragmentController.java
@@ -1,7 +1,37 @@
-package src.com.team08storyapp.test;
+package com.team08storyapp.test;
 
 import junit.framework.TestCase;
+import com.team08storyapp.Story;
+import com.team08storyapp.StoryFragment;
 
 public class testStoryFragmentController extends TestCase {
 
+	
+	
+	//SetUp sets up variables that are to be used in the test cases
+	protected void setUp(){
+		
+	}
+	
+	//addAnnotation adds an annotation to a StoryFragment Object.
+	public void testAddAnnotation(){
+		
+	}
+	
+	//addChoice adds a choice to a StoryFragment Object.
+	public void testAddChoice(){
+		
+	}
+	
+	//addImage adds an image to a StoryFragment Object.
+	public void testAddImage(){
+		
+	}
+	
+	//updateStoryFragment updates a fragment that has just been modified.
+	public void testUpdateStoryFragment(){
+		
+	}
+	
+	
 }
