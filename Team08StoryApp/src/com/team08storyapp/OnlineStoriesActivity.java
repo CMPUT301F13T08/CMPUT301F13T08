792f57d2c9152dce8e8ecaf3dea0293c9da36a13
Jarrett <toll@ualberta.ca>
2013-10-27 16:20:07 -0600

    Added context menu for OnlineStoriesActivity

--- Team08StoryApp/src/com/team08storyapp/OnlineStoriesActivity.java
+++ Team08StoryApp/src/com/team08storyapp/OnlineStoriesActivity.java
@@ -3,9 +3,19 @@ package com.team08storyapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class OnlineStoriesActivity extends ListActivity {
+	
+	private static final int DOWNLOAD_ID = 1;
+	private static final int READ_ID = 2;
+	public int position;
+	public AdapterContextMenuInfo info;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,25 @@ public class OnlineStoriesActivity extends ListActivity {
 		getMenuInflater().inflate(R.menu.online_stories, menu);
 		return true;
 	}
+	
+	
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, DOWNLOAD_ID, 0, R.string.download_menu);
+		menu.add(0,READ_ID, 0, R.string.read_menu);
+	}
 
+	public boolean onContextItemSelected(MenuItem item){
+		info = (AdapterContextMenuInfo) item.getMenuInfo();
+		position = info.position;
+		switch(item.getItemId()){
+		case DOWNLOAD_ID:
+			
+			
+		case READ_ID:
+			
+			default:
+				return super.onContextItemSelected(item);
+		}
+	}
 }
