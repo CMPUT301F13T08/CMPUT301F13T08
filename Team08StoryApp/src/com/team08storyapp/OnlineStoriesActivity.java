1156a0638f6edb68dc28b9117b41346be0a2fa44
Jarrett <toll@ualberta.ca>
2013-10-27 13:03:55 -0600

    Added OnlineStoriesActivity with layout.

--- Team08StoryApp/src/com/team08storyapp/OnlineStoriesActivity.java
+++ Team08StoryApp/src/com/team08storyapp/OnlineStoriesActivity.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class StoryListActivity extends Activity {
+public class OnlineStoriesActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_story_list);
+		setContentView(R.layout.activity_online_stories);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.story_list, menu);
+		getMenuInflater().inflate(R.menu.online_stories, menu);
 		return true;
 	}
 
