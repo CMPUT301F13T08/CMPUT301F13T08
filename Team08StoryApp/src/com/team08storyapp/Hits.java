123c61d4f8effd04061e62130d7c9e8a846aa02c
mep6 <mep6@ualberta.ca>
2013-10-27 16:59:31 -0600

    Added files for ElastiSearch

---
+++ Team08StoryApp/src/com/team08storyapp/Hits.java
@@ -0,0 +1,15 @@
+package com.team08storyapp;
+
+import java.util.Collection;
+
+public class Hits<T> {
+    int total;
+    double max_score;
+    Collection<ElasticSearchResponse<T>> hits;
+    public Collection<ElasticSearchResponse<T>> getHits() {
+        return hits;
+    }
+    public String toString() {
+        return (super.toString()+","+total+","+max_score+","+hits);
+    }
+}
