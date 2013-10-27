ca7ecd5cdfe95c9417d367bb9ee6b58c638b9b60
mep6 <mep6@ualberta.ca>
2013-10-27 14:48:03 -0600

    added skeleton photoController class

---
+++ Team08StoryApp/src/com/team08storyapp/PhotoController.java
@@ -0,0 +1,17 @@
+package com.team08storyapp;
+
+
+
+public class PhotoController {
+
+	public static Photo takePhoto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Photo uploadPhoto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
