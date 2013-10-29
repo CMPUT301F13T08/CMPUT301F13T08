928876923617a981766ec7076c031ced71c558b0
AliceM <mingxun@ualberta.ca>
2013-10-28 22:16:49 -0600

    OfflineStoriesActivity and OnlineStoriesActivity are good to work. 1) list stories with search bar on the top. 2) refresh the list whenever a search is carried out. And press the search button on search bar when searchText is null or empty, all online/offline stories will be displayed.

--- Team08StoryApp/src/com/team08storyapp/OfflineStoriesActivity.java
+++ Team08StoryApp/src/com/team08storyapp/OfflineStoriesActivity.java
@@ -1,22 +1,110 @@
 package com.team08storyapp;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class OfflineStoriesActivity extends Activity {
+public class OfflineStoriesActivity extends ListActivity {
+	
+	public int position;
+	public AdapterContextMenuInfo info;
+	private static final boolean onUpdate = true;
+	private static final boolean onCreate = false;
+	
+	private FileHelper fHelper;
+	private View header;
+	private String searchText;
+	private EditText et;
+	
+	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_offline_stories);
+		
+		fHelper = new FileHelper(this, 0);
+		header = getLayoutInflater().inflate(R.layout.header_search, null);
+		
+		try {
+			fillData(fHelper.getOfflineStories(), onCreate);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Button searchButton = (Button) header.findViewById(R.id.searchButton);
+		et = (EditText) header.findViewById(R.id.searchText);
+		
+		searchButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				searchText = et.getText().toString();			
+				if(searchText != null && searchText!= ""){
+					System.out.println(searchText);
+					ArrayList<Story> result = fHelper.searchOfflineStories(searchText);
+					fillData(result, onUpdate);
+				}else{
+					try {
+						fillData(fHelper.getOfflineStories(), onUpdate);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		});
+		
+		registerForContextMenu(getListView());
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.offline_stories, menu);
 		return true;
 	}
+	
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);       
+        // TODO: read the story
+    }
+
+	
+	
+	public void fillData(ArrayList<Story> sList, boolean update){
+		ArrayList<String> lList = new ArrayList<String>();
+		for(int i = 0; i < sList.size(); i++){
+			lList.add(sList.get(i).getTitle() + " \nBy: "+ sList.get(i).getAuthor());
+		}
+		String[] lArray = new String[lList.size()];
+		lArray = lList.toArray(lArray);
+		ListView lv = getListView();
+		if(!update){
+			lv.addHeaderView(header);	
+		}
+		setListAdapter(new ArrayAdapter<String>(this,R.layout.stories_row, lArray));
+
+
+	}
+
 
 }
--- Team08StoryApp/src/com/team08storyapp/OnlineStoriesActivity.java
+++ Team08StoryApp/src/com/team08storyapp/OnlineStoriesActivity.java
@@ -1,26 +1,90 @@
 package com.team08storyapp;
 
-import android.os.Bundle;
-import android.app.Activity;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.example.filehelperv0.FileHelper;
+import com.example.filehelperv0.R;
+import com.example.filehelperv0.Story;
+
 import android.app.ListActivity;
+import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class OnlineStoriesActivity extends ListActivity {
+
+	// make sure to assign the "menu" thing with menu's value, otherwise
+	// contextmenu won't respond.
+	// private FileHelper fHelper; should be ESHelper
 	
-	private static final int DOWNLOAD_ID = 1;
-	private static final int READ_ID = 2;
 	public int position;
 	public AdapterContextMenuInfo info;
+	
+	private static final int DOWNLOAD_ID = Menu.FIRST;
+	private static final int READ_ID = Menu.FIRST+1;
+	private static final boolean onUpdate = true;
+	private static final boolean onCreate = false;
+	
+	private ESHelper esHelper;
+	private View header;
+	private String searchText;
+	private EditText et;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_online_stories);
+		
+		esHelper = new ESHelper();
+		header = getLayoutInflater().inflate(R.layout.header_search, null);
+		
+		try {
+			fillData(esHelper.getOnlineStories(), onCreate);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Button searchButton = (Button) header.findViewById(R.id.searchButton);
+		et = (EditText) header.findViewById(R.id.searchText);
+		
+		searchButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				searchText = et.getText().toString();			
+				if(searchText != null && searchText!= ""){
+					System.out.println(searchText);
+					ArrayList<Story> result = esHelper.searchOnlineStories(searchText);
+					fillData(result, onUpdate);
+				}else{
+					try {
+						fillData(esHelper.getOnlineStories(), onUpdate);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		});
+		
+		registerForContextMenu(getListView());
 	}
 
 	@Override
@@ -50,4 +114,23 @@ public class OnlineStoriesActivity extends ListActivity {
 				return super.onContextItemSelected(item);
 		}
 	}
+	
+	public void fillData(ArrayList<Story> sList, boolean update){
+		ArrayList<String> lList = new ArrayList<String>();
+		for(int i = 0; i < sList.size(); i++){
+			lList.add(sList.get(i).getTitle() + " \nBy: "+ sList.get(i).getAuthor());
+		}
+		String[] lArray = new String[lList.size()];
+		lArray = lList.toArray(lArray);
+		ListView lv = getListView();
+		if(!update){
+			lv.addHeaderView(header);	
+		}
+		setListAdapter(new ArrayAdapter<String>(this,R.layout.stories_row, lArray));
+
+
+	}
+	
+	
+
 }
