e2b751b5c8e47442a7129b7bb6bd57d1b7944f84
AliceM <mingxun@ualberta.ca>
2013-10-31 12:12:43 -0600

    1) fix an careless logic error in FileHelper 2) Link MystoriesActicity to MainActivity

--- Team08StoryApp/src/com/team08storyapp/MyStoriesActivity.java
+++ Team08StoryApp/src/com/team08storyapp/MyStoriesActivity.java
@@ -15,11 +15,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MyStoriesActivity extends ListActivity {
-    
 
     public int position;
     public AdapterContextMenuInfo info;
@@ -32,6 +32,9 @@ public class MyStoriesActivity extends ListActivity {
     private ESHelper esHelper;
     private FileHelper fHelper;
     private View footerCreate;
+    private View header;
+    private EditText et;
+    private String searchText;
     private ListView lv;
     private Story currentStory;
 
@@ -40,14 +43,48 @@ public class MyStoriesActivity extends ListActivity {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_story_list);
 	lv = (ListView) findViewById(android.R.id.list);
-	footerCreate = getLayoutInflater().inflate(R.layout.footer_create_button, null);
-	Button createButton = (Button) footerCreate.findViewById(R.id.createButton);
+	header = getLayoutInflater().inflate(R.layout.header_search, null);
+	Button searchButton = (Button) header.findViewById(R.id.searchButton);
+	et = (EditText) header.findViewById(R.id.searchText);
+	footerCreate = getLayoutInflater().inflate(
+		R.layout.footer_create_button, null);
+	Button createButton = (Button) footerCreate
+		.findViewById(R.id.createButton);
 
 	fHelper = new FileHelper(this, 1);
 	esHelper = new ESHelper();
-	
+
 	try {
-	    fillData(fHelper.getOfflineStories(), onCreate);
+	    Story s1 = new Story(11, "To the End", "Alan Cumming");
+	    Story s2 = new Story(12, "Watchmen", "Alan Moore");
+	    Story s3 = new Story(13, "SlaughterHouse-Five", "");
+	    Story s4 = new Story(14, "Animal Farm", "George Owell");
+	    Story s5 = new Story(15, "The art of computer programming",
+		    "Donald Knuth");
+	    Story s6 = new Story(16, "Software and hardware", "D. Patterson");
+	    Story s7 = new Story(17, "Macbeth", "William Shakespeare");
+	    Story s8 = new Story(18, "King Lear", "William Shakespeare");
+	    Story s9 = new Story(19, "The Fall of the House of Usher",
+		    "Edgar Allan Poe");
+	    Story s10 = new Story(20, "Annabel Lee", "Edgar Allan Poe");
+	    Story[] slist = { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10 };
+	    for (Story s : slist) {		
+		fHelper.addOfflineStory(s);
+	    }
+	} catch (FileNotFoundException e1) {
+	    // TODO Auto-generated catch block
+	    e1.printStackTrace();
+	} catch (IOException e1) {
+	    // TODO Auto-generated catch block
+	    e1.printStackTrace();
+	}
+
+	try {
+	    ArrayList<Story> temp = fHelper.getOfflineStories();
+	    for(Story s: temp){
+		System.out.println(s.getTitle());
+	    }
+	    fillData(temp, onCreate);
 	} catch (FileNotFoundException e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
@@ -56,13 +93,34 @@ public class MyStoriesActivity extends ListActivity {
 	    e.printStackTrace();
 	}
 
-	/*createButton.setOnClickListener(new View.OnClickListener() {
+	searchButton.setOnClickListener(new View.OnClickListener() {
 
 	    @Override
 	    public void onClick(View v) {
-		//TODO: start a create a new story activity
+		searchText = et.getText().toString();
+		if (searchText != null && searchText != "") {
+		    fillData(fHelper.searchOfflineStories(searchText), onUpdate);
+		} else {
+		    try {
+			fillData(fHelper.getOfflineStories(), onUpdate);
+		    } catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    }
+		}
+
+	    }
 	});
-	*/
+
+	/*
+	 * createButton.setOnClickListener(new View.OnClickListener() {
+	 * 
+	 * @Override public void onClick(View v) { //TODO: start a create a new
+	 * story activity });
+	 */
 
 	registerForContextMenu(getListView());
     }
@@ -77,7 +135,7 @@ public class MyStoriesActivity extends ListActivity {
     public void onCreateContextMenu(ContextMenu menu, View v,
 	    ContextMenuInfo menuInfo) {
 	super.onCreateContextMenu(menu, v, menuInfo);
-	menu.add(0, PUBLISH_ID, 0, R.string.download_menu);
+	menu.add(0, PUBLISH_ID, 0, R.string.publish_menu);
 	menu.add(0, READ_ID, 0, R.string.read_menu);
     }
 
@@ -85,20 +143,22 @@ public class MyStoriesActivity extends ListActivity {
 	info = (AdapterContextMenuInfo) item.getMenuInfo();
 	position = info.position;
 	// following 4 lines will display the information on selected item.
-	Story selectedValue = (Story) lv.getAdapter().getItem(position);	
-	System.out.println(selectedValue.getTitle());
-	System.out.println(selectedValue.getAuthor());
-	System.out.println(selectedValue.getStoryId());
-	
+	Story currentStory = (Story) lv.getAdapter().getItem(position);
+	System.out.println(currentStory.getTitle());
+	System.out.println(currentStory.getAuthor());
+	System.out.println(currentStory.getStoryId());
+
 	switch (item.getItemId()) {
 	case PUBLISH_ID:
 	    // TODO: call esHelper to publish
-	    if(esHelper.addOnlineStory(selectedValue)){
-		Toast.makeText(getApplicationContext(), 
-                        "Your Story is Successfully Published", Toast.LENGTH_LONG).show();
-	    }else{
-		Toast.makeText(getApplicationContext(), 
-                        "Publish Error. Internect Connection Error", Toast.LENGTH_LONG).show();
+	    if (esHelper.addOnlineStory(currentStory)) {
+		Toast.makeText(getApplicationContext(),
+			"Your Story is Successfully Published",
+			Toast.LENGTH_LONG).show();
+	    } else {
+		Toast.makeText(getApplicationContext(),
+			"Publish Error. Internect Connection Error",
+			Toast.LENGTH_LONG).show();
 	    }
 
 	case READ_ID:
@@ -126,7 +186,8 @@ public class MyStoriesActivity extends ListActivity {
 
     public void fillData(ArrayList<Story> sList, boolean update) {
 	if (!update) {
-	    lv.addHeaderView(footerCreate);
+	    lv.addHeaderView(header);
+	    lv.addFooterView(footerCreate);
 	}
 	lv.setAdapter(new StoryInfoAdapter(this, android.R.id.list, sList));
     }
