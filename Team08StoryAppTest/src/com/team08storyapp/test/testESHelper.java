1d2dc4c2b1ccb75976c34340413aa01581c860da
mep6 <mep6@ualberta.ca>
2013-10-31 15:07:23 -0600

    fixed issue with choses for the sample story used for testing

--- Team08StoryAppTest/src/com/team08storyapp/test/testESHelper.java
+++ Team08StoryAppTest/src/com/team08storyapp/test/testESHelper.java
@@ -37,65 +37,66 @@ public class testESHelper extends TestCase {
 	StoryFragment storyFragment1 = new StoryFragment(1);
 	storyFragment1
 		.setStoryText("Like any other day, Amara decided to take her dog out for a walk. She left the house and exited the yard.");
-	choices.add(new Choice(2, 1,
+	ArrayList<Choice> choices1 = new ArrayList<Choice>();
+	choices1.add(new Choice(2, 1,
 		"Amara turned south heading for her friend’s house."));
-	choices.add(new Choice(7, 2,
+	choices1.add(new Choice(7, 2,
 		"Amara turned north heading for the store."));
-	choices.add(new Choice(3, 3,
+	choices1.add(new Choice(3, 3,
 		"Amara turned east heading for the river valley."));
-	storyFragment1.setChoices(choices);
+	storyFragment1.setChoices(choices1);
 	storyFragmentList.add(storyFragment1);
 
 	// Story Fragment 2
 	StoryFragment storyFragment2 = new StoryFragment(2);
 	storyFragment2
 		.setStoryText("As Amara walked toward her friend’s house her dog started acting up. It pulled this way and that way. Then it stopped and started barking at a bush.");
-	choices.clear();
-	choices.add(new Choice(4, 1, "In the bush, Amara found a small boy."));
-	choices.add(new Choice(5, 2,
+	ArrayList<Choice> choices2 = new ArrayList<Choice>();
+	choices2.add(new Choice(4, 1, "In the bush, Amara found a small boy."));
+	choices2.add(new Choice(5, 2,
 		"In the bush, Amara found a hundred dollar bill."));
-	storyFragment2.setChoices(choices);
+	storyFragment2.setChoices(choices2);
 	storyFragmentList.add(storyFragment2);
 
 	// Story Fragment 3
 	StoryFragment storyFragment3 = new StoryFragment(3);
 	storyFragment3
 		.setStoryText("Amara gets to the river valley and decides to take a path she has yet to explore. As Amara walks along she comes across a cave in the side of the valley.");
-	choices.clear();
-	choices.add(new Choice(8, 1,
+	ArrayList<Choice> choices3 = new ArrayList<Choice>();
+	choices3.add(new Choice(8, 1,
 		"Amara decides to take a peek in the cave."));
-	choices.add(new Choice(9, 2,
+	choices3.add(new Choice(9, 2,
 		"Amara thinks the cave is creepy and continues walking."));
-	storyFragment3.setChoices(choices);
+	storyFragment3.setChoices(choices3);
 	storyFragmentList.add(storyFragment3);
 
 	// Story Fragment 4
 	StoryFragment storyFragment4 = new StoryFragment(4);
 	storyFragment4
 		.setStoryText("At first Amara was startled by the boy. Then she realized he was crying softly. So she asked the boy what’s wrong. The boy was lost.");
-	choices.clear();
-	choices.add(new Choice(10, 1,
+	ArrayList<Choice> choices4 = new ArrayList<Choice>();
+	choices4.add(new Choice(10, 1,
 		"Amara decides to help the boy find his way home."));
-	choices.add(new Choice(11, 2, "Amara is uncertain what to do."));
-	storyFragment4.setChoices(choices);
+	choices4.add(new Choice(11, 2, "Amara is uncertain what to do."));
+	storyFragment4.setChoices(choices4);
 	storyFragmentList.add(storyFragment4);
 
 	// Story Fragment 5
 	StoryFragment storyFragment5 = new StoryFragment(5);
 	storyFragment5
 		.setStoryText("Amara picked up the bill and looked around. There was no one around and therefore anyone to claim the money. So Amara decided to keep the bill and continue on to her friend’s house.");
-	choices.clear();
-	choices.add(new Choice(6, 1, "Continue"));
-	storyFragment5.setChoices(choices);
+	ArrayList<Choice> choices5 = new ArrayList<Choice>();
+	choices5.add(new Choice(6, 1, "Continue"));
+	storyFragment5.setChoices(choices5);
 	storyFragmentList.add(storyFragment5);
 
 	// Story Fragment 6
 	StoryFragment storyFragment6 = new StoryFragment(6);
 	storyFragment6
 		.setStoryText("When Amara got to her friend’s house she explained her excitement at finding one hundred dollars in a bush, all thanks to her dog. Her friend suggests she should reward her dog a treat with part of the money.");
-	choices.clear();
-	choices.add(new Choice(7, 1, "Continue"));
-	storyFragment6.setChoices(choices);
+	ArrayList<Choice> choices6 = new ArrayList<Choice>();
+	choices6.add(new Choice(7, 1, "Continue"));
+	storyFragment6.setChoices(choices6);
 	storyFragmentList.add(storyFragment6);
 
 	// Story Fragment 7
@@ -108,9 +109,9 @@ public class testESHelper extends TestCase {
 	StoryFragment storyFragment8 = new StoryFragment(8);
 	storyFragment8
 		.setStoryText("Amara steps towards the cave, as a little boy crawls out of it.");
-	choices.clear();
-	choices.add(new Choice(4, 1, "Continue"));
-	storyFragment8.setChoices(choices);
+	ArrayList<Choice> choices8 = new ArrayList<Choice>();
+	choices8.add(new Choice(4, 1, "Continue"));
+	storyFragment8.setChoices(choices8);
 	storyFragmentList.add(storyFragment8);
 
 	// Story Fragment 9
@@ -123,9 +124,9 @@ public class testESHelper extends TestCase {
 	StoryFragment storyFragment10 = new StoryFragment(10);
 	storyFragment10
 		.setStoryText("The boy says he knows his phone number and his mom is home but has no way to call her. Amara pulls out her cell and dials the number. She explains she found the boy and where she is.");
-	choices.clear();
-	choices.add(new Choice(12, 1, "Continue"));
-	storyFragment10.setChoices(choices);
+	ArrayList<Choice> choices9 = new ArrayList<Choice>();
+	choices9.add(new Choice(12, 1, "Continue"));
+	storyFragment10.setChoices(choices9);
 	storyFragmentList.add(storyFragment10);
 
 	// Story Fragment 11
