03a8d7de7b3313af9a0ccf23ea28c6e24537674f
Jiawei Shen <jishent@gmail.com>
2013-11-01 19:22:36 -0600

    PhotoController Update

--- Team08StoryApp/src/com/team08storyapp/PhotoController.java
+++ Team08StoryApp/src/com/team08storyapp/PhotoController.java
@@ -1,11 +1,39 @@
 package com.team08storyapp;
 
-public class PhotoController {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+
+public class PhotoController extends Activity {
+
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	Uri imageFileUri;
 	
-	public static Photo takePhoto() {
-		// TODO Auto-generated method stub
-		return null;
+	public void takePhoto(){
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // Image captured and saved to fileUri specified in the Intent
+	            Toast.makeText(this, "Image saved to:\n" +
+	                     data.getData(), Toast.LENGTH_LONG).show();
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
+	}
+	
+	
+
 
 	public static Photo uploadPhoto() {
 		// TODO Auto-generated method stub
