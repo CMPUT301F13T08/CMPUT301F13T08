6900c6867d8622fcb68db4fa271daf9620f7aaa1
Jiawei Shen <jishent@gmail.com>
2013-11-02 13:49:07 -0600

    Update on StoryFragmentController

--- Team08StoryApp/src/com/team08storyapp/StoryFragmentController.java
+++ Team08StoryApp/src/com/team08storyapp/StoryFragmentController.java
@@ -1,18 +1,20 @@
 package com.team08storyapp;
 
 
+
+
 public class StoryFragmentController {
 	
-	public void addAnnotation(Annotation annotation){
-		// TODO Auto-generated method stub
+	public void addAnnotation(Annotation annotation, StoryFragment storyfragment){
+        storyfragment.getAnnotations().add(annotation);
 	}
 	
-	public void addChoice(Choice choice){
-		// TODO Auto-generated method stub
+	public void addChoice(Choice choice, StoryFragment storyfragment){
+		storyfragment.getChoices().add(choice);
 	}
 	
-	public void addImage (Photo photo){
-		// TODO Auto-generated method stub
+	public void addImage (Photo photo, StoryFragment storyfragment){
+		storyfragment.getPhotos().add(photo);
 	}
 	
 	public boolean updateStoryFragment(StoryFragment storyfragment){
