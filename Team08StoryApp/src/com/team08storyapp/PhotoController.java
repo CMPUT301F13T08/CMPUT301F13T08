64ad3541c0beee3a810cff098a408fff6981b16c
Jiawei Shen <jishent@gmail.com>
2013-11-02 00:44:21 -0600

    PhotoController update
    The app can take a picture using default camera intent and stores it
    into memory. It can discard the current preview and take another one.
    Now trying to figure out how to convert to bitmap.

--- Team08StoryApp/src/com/team08storyapp/PhotoController.java
+++ Team08StoryApp/src/com/team08storyapp/PhotoController.java
@@ -32,12 +32,59 @@ public class PhotoController extends Activity {
 	    }
 	}
 	
+	/*public void takePhoto(){
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
 	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+	{  
+	    //Check that request code matches ours:
+	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) 
+	    {
+	        //Get our saved file into a bitmap object:
+	       File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+	       Bitmap bitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+	    }
+	}
+	
+	public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) 
+	{    
+	    //First decodes with inJustDecodeBounds to check dimensions
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(path, options);
+	 
+	    //Calculate inSampleSize, Raw height and width of image
+	    final int height = options.outHeight;
+	    final int width = options.outWidth;
+	    options.inPreferredConfig = Bitmap.Config.RGB_565;
+	    int inSampleSize = 1;
+	 
+	    if (height > reqHeight) 
+	    {
+	        inSampleSize = Math.round((float)height / (float)reqHeight);
+	    }
+	    int expectedWidth = width / inSampleSize;
+	 
+	    if (expectedWidth > reqWidth) 
+	    {
+	        inSampleSize = Math.round((float)width / (float)reqWidth);
+	    }
+	 
+	    options.inSampleSize = inSampleSize;
+	 
+	    // Decode bitmap with inSampleSize set
+	    options.inJustDecodeBounds = false;
+	 
+	    return BitmapFactory.decodeFile(path, options);
+	}*/
 
 
-	public static Photo uploadPhoto() {
-		// TODO Auto-generated method stub
-		return null;
+	public void uploadPhoto() {
+		
 	}
 
 }
