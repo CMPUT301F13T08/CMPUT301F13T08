8ad336726cd17abf3d213a14d06514601dc53ee0
Jiawei Shen <Jiawei@shen-jiaweis-macbook-pro-2.local>
2013-11-03 23:58:25 -0700

    Clean up.

--- Team08StoryApp/src/com/team08storyapp/PhotoController.java
+++ Team08StoryApp/src/com/team08storyapp/PhotoController.java
@@ -42,13 +42,10 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.widget.Toast;
 
-
 public class PhotoController extends Activity {
 
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	private static final int SELECT_PHOTO = 200;
 	Uri imageFileUri;
-	
 	
 	public void takePhoto(){
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -63,90 +60,5 @@ public class PhotoController extends Activity {
 	    startActivityForResult(
 			Intent.createChooser(pickIntent, "Select Picture"), 1);
 	}
-	
-	
-	/*
-	protected void onActivityResult(int requestCode, int resultCode, Intent data){
-	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-	        if (resultCode == RESULT_OK) {
-	            // Image captured and saved to fileUri specified in the Intent
-	            Toast.makeText(this, "Image saved to:\n" +
-	                     data.getData(), Toast.LENGTH_LONG).show();
-	        } else if (resultCode == RESULT_CANCELED) {
-	            // User cancelled the image capture
-	        } else {
-	            // Image capture failed, advise user
-	        }
-	    }
-	    if (requestCode == SELECT_PHOTO){
-	            Uri selectedImage = data.getData();
-	            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-	            Cursor cursor = getContentResolver().query(
-	                               selectedImage, filePathColumn, null, null, null);
-	            cursor.moveToFirst();
-
-	            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-	            String filePath = cursor.getString(columnIndex);
-	            cursor.close();
-
-
-	            @SuppressWarnings("unused")
-				Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
-	            }
-	        
-	    
-	}
-	
-	/*public void takePhoto(){
-		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-		File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-	}
-	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
-	{  
-	    //Check that request code matches ours:
-	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) 
-	    {
-	        //Get our saved file into a bitmap object:
-	       File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
-	       Bitmap bitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
-	    }
-	}
-	
-	public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) 
-	{    
-	    //First decodes with inJustDecodeBounds to check dimensions
-	    final BitmapFactory.Options options = new BitmapFactory.Options();
-	    options.inJustDecodeBounds = true;
-	    BitmapFactory.decodeFile(path, options);
-	 
-	    //Calculate inSampleSize, Raw height and width of image
-	    final int height = options.outHeight;
-	    final int width = options.outWidth;
-	    options.inPreferredConfig = Bitmap.Config.RGB_565;
-	    int inSampleSize = 1;
-	 
-	    if (height > reqHeight) 
-	    {
-	        inSampleSize = Math.round((float)height / (float)reqHeight);
-	    }
-	    int expectedWidth = width / inSampleSize;
-	 
-	    if (expectedWidth > reqWidth) 
-	    {
-	        inSampleSize = Math.round((float)width / (float)reqWidth);
-	    }
-	 
-	    options.inSampleSize = inSampleSize;
-	 
-	    // Decode bitmap with inSampleSize set
-	    options.inJustDecodeBounds = false;
-	 
-	    return BitmapFactory.decodeFile(path, options);
-	}*/
-
 }
 
