2de9cd7d4d097ec291ce5744d8a1fe9c4ccb2969
Jiawei Shen <Jiawei@uwsclient-146-70.uws.ualberta.ca>
2013-11-04 14:38:07 -0700

    Format Update.

--- Team08StoryApp/src/com/team08storyapp/StoryFragmentController.java
+++ Team08StoryApp/src/com/team08storyapp/StoryFragmentController.java
@@ -32,22 +32,22 @@ Retrieved Oct. 29, 2013
 package com.team08storyapp;
 
 public class StoryFragmentController {
-	
-	public void addAnnotation(Annotation annotation, StoryFragment storyfragment){
-        storyfragment.getAnnotations().add(annotation);
-	}
-	
-	public void addChoice(Choice choice, StoryFragment storyfragment){
-		storyfragment.getChoices().add(choice);
-	}
-	
-	public void addImage (Photo photo, StoryFragment storyfragment){
-		storyfragment.getPhotos().add(photo);
-	}
-	
-	public boolean updateStoryFragment(StoryFragment storyfragment){
-		// TODO Auto-generated method stub
-		return false;
-	}
+
+    public void addAnnotation(Annotation annotation, StoryFragment storyfragment) {
+	storyfragment.getAnnotations().add(annotation);
+    }
+
+    public void addChoice(Choice choice, StoryFragment storyfragment) {
+	storyfragment.getChoices().add(choice);
+    }
+
+    public void addImage(Photo photo, StoryFragment storyfragment) {
+	storyfragment.getPhotos().add(photo);
+    }
+
+    public boolean updateStoryFragment(StoryFragment storyfragment) {
+	// TODO Auto-generated method stub
+	return false;
+    }
 
 }
