395b1c332e0af61bbc16a63fa5a9bdfe015f9988
AliceM <mingxun@ualberta.ca>
2013-11-04 15:06:39 -0700

    deleted unrelated tests

--- Team08StoryApp/src/com/team08storyapp/MyStoryFragmentActivity.java
+++ Team08StoryApp/src/com/team08storyapp/MyStoryFragmentActivity.java
@@ -5,7 +5,7 @@ Alice Wu, Ana Marcu, Michele Paulichuk, Jarrett Toll, Jiawei Shen.
 
 LICENSE
 =======
-Copyright  ©  2013 Alice Wu, Ana Marcu, Michele Paulichuk, Jarrett Toll, Jiawei Shen,  
+Copyright  ï¿½  2013 Alice Wu, Ana Marcu, Michele Paulichuk, Jarrett Toll, Jiawei Shen,  
 Free Software Foundation, Inc., Marky Mark  License GPLv3+: GNU
 GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 This program is free software: you can redistribute it and/or modify it under the terms of 
@@ -335,6 +335,7 @@ public class MyStoryFragmentActivity extends Activity {
 		    currentStory.getStoryFragments().set(
 			    currentStoryFragmentIndex, currentStoryFragment);
 		    System.out.println("SWAP FRAGMENT DONE");
+		    
 		    try {
 			fHelper.updateOfflineStory(currentStory);
 			System.out.println("Test currentStoryId:" + currentStoryId);
