2fa7e0cb7f0f1ed2947bea63bfca00dedab328e5
marcugit <marcu@ualberta.ca>
2013-11-04 14:15:20 -0700

    Update testStoryController.java

--- Team08StoryAppTest/src/com/team08storyapp/test/testStoryController.java
+++ Team08StoryAppTest/src/com/team08storyapp/test/testStoryController.java
@@ -19,15 +19,8 @@ public class testStoryController extends TestCase {
 	StoryFragment secondStoryFragment;
 	ArrayList<StoryFragment> storyFragmentList = null;
 		
-	/* The testAddStoryFragment method tests adding a story fragment to an existing story. If 
-	 * the story fragment is added successfully the method call to esHelper.addStoryFragment 
-	 * should return true. This is related to user story # 5.
-	 **/
-	public void testAddStoryFragment(){
-		assertTrue(StoryController.addStoryFragment(addStoryFragment));
-	}
-	
-	protected void setUp(){		
+		
+		protected void setUp(){		
 	    	
 	    	// set the next fragment id to 2 - see testReadStoryFragment
 	    	nextStoryFragmentId = 2;
@@ -40,12 +33,24 @@ public class testStoryController extends TestCase {
 		
 		
 	}
+		
+		
+	/* The testAddStoryFragment method tests adding a story fragment to an existing story. If 
+	 * the story fragment is added successfully the method call to esHelper.addStoryFragment 
+	 * should return true. This is related to user story # 5.
+	 **/
+	public void testAddStoryFragment(){
+		assertTrue(StoryController.addStoryFragment(addStoryFragment));
+	}
 	
+
 	
+	// call the method readStoryFragment() of story controller, to retrieve a fragment from a list, by id
+	//the returned fragment should be the same as the fragment in the storyFragmentList with id 2
 	public void testReadStoryFragment(){
-	    	// call the method readStoryFragment() of story controller, to retrieve a fragment from a list, by id
+	    	
 	    	retrievedStoryFragment = StoryController.readStoryFragment(storyFragmentList, nextStoryFragmentId);
-	    	//the returned fragment should be the same as the fragment in the storyFragmentList with id 2
+	    	
 	    	assertEquals(retrievedStoryFragment, secondStoryFragment);
 	    	
 	}
