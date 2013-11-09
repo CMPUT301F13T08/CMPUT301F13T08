dec76f43a8cc4ddec0720f33c592b02862d4c158
Jarrett <toll@ualberta.ca>
2013-11-09 16:35:42 -0700

    Added menu for EditFragmentActivity.

--- Team08StoryApp/src/com/team08storyapp/EditFragmentActivity.java
+++ Team08StoryApp/src/com/team08storyapp/EditFragmentActivity.java
@@ -3,6 +3,7 @@ package com.team08storyapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditFragmentActivity extends Activity {
 
@@ -15,8 +16,30 @@ public class EditFragmentActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit, menu);
+		getMenuInflater().inflate(R.menu.edit_fragment, menu);
 		return true;
 	}
 
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.camIllus:
+	            return true;
+	            
+	        case R.id.camGallery:
+	            return true;
+	            
+	        case R.id.addChoice:
+	        	return true;
+	        	
+	        case R.id.save:
+	        	return true;
+	        	
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 }
