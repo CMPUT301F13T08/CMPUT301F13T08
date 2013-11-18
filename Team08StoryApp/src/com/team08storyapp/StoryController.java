57ad6e978e589301843b9132a3e97b9da34739c5
Jiawei Shen <Jiawei@Shen-Jiaweis-MacBook-Pro-2.local>
2013-11-17 21:28:46 -0700

    Starting on randomChoice method

--- Team08StoryApp/src/com/team08storyapp/StoryController.java
+++ Team08StoryApp/src/com/team08storyapp/StoryController.java
@@ -149,4 +149,18 @@ public class StoryController {
 	Random rand = new Random();
 	return rand.nextInt(storyListSize);
     }
+    
+    /**
+     * This method takes in an Array list of choices and returns a random 
+     * story fragment that the current story fragment might go to.
+     * 
+     * Example: If
+     * 
+     */
+    public static StoryFragment randomChoice(ArrayList<Choice> Choices){
+	StoryFragment randomFragment = null;
+	
+	return randomFragment;
+	
+    }
 }
