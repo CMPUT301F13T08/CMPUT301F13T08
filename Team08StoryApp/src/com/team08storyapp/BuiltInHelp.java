921d43ae89baa1e6783faa71b77ef861101337d7
mep6 <mep6@ualberta.ca>
2013-11-19 15:50:48 -0700

    Working built-in help for main activity screen.

--- Team08StoryApp/src/com/team08storyapp/BuiltInHelp.java
+++ Team08StoryApp/src/com/team08storyapp/BuiltInHelp.java
@@ -2,6 +2,8 @@ package com.team08storyapp;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ContextWrapper;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,30 +16,39 @@ public class BuiltInHelp {
 
     public BuiltInHelp(Activity activity) {
 	this.activity = activity;
-	setMainMenuHelp();
+	if (activity.getComponentName().getClassName()
+		.contentEquals("com.team08storyapp.MainActivity")) {
+	    setMainMenuHelp();
+	}
     }
 
-    public void showDialog(){
+    public void showDialog() {
 	final Dialog helpDialog = new Dialog(activity);
-	    helpDialog.setContentView(R.layout.help_dialog);
-	    TextView helptext = (TextView) helpDialog
-		    .findViewById(R.id.helptextView);
-	    helpDialog.setTitle(helpTitle);
-	    helptext.setText(helpText);
-	    Button okayButton = (Button) helpDialog.findViewById(R.id.okayButton);
-	    okayButton.setOnClickListener(new View.OnClickListener() {
-		 
-            @Override
-            public void onClick(View view) {
-                helpDialog.dismiss();
-            }
+	helpDialog.setContentView(R.layout.help_dialog);
+	TextView helptext = (TextView) helpDialog
+		.findViewById(R.id.helptextView);
+	helpDialog.setTitle(helpTitle);
+	helptext.setText(Html.fromHtml(helpText));
+	Button okayButton = (Button) helpDialog.findViewById(R.id.okayButton);
+	okayButton.setOnClickListener(new View.OnClickListener() {
 
-        });
-	    helpDialog.show();	    
+	    @Override
+	    public void onClick(View view) {
+		helpDialog.dismiss();
+	    }
+
+	});
+	helpDialog.show();
     }
+
     private void setMainMenuHelp() {
 	this.helpTitle = "Main Menu Help";
-	this.helpText = "<b>Read Online Stories</b><br>When clicked will lead the user to the Online Stories screen.";
+	this.helpText = "<B>Read Online Stories Button</B>"
+		+ "<br>Click to display the list of Stories available to read from the online webservice.<br>"
+		+ "<br><B>Read Downloaded Stories Button</B>"
+		+ "<br>Click to display the list of Stories available to read that are local stored on the device.<br>"
+		+ "<br><B>My Stories Button</B>"
+		+ "<br>Click to display the list of Stories you are creating.";
     }
-    
+
 }
