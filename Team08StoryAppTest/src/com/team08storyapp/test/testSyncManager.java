f5f699c8c1dd79ef9da7c61ae3e9001a79118e7d
AliceM <mingxun@ualberta.ca>
2013-11-20 14:12:31 -0700

    SyncManager test

---
+++ Team08StoryAppTest/src/com/team08storyapp/test/testSyncManager.java
@@ -0,0 +1,18 @@
+package com.team08storyapp.test;
+
+import junit.framework.TestCase;
+
+import com.team08storyapp.SyncManager;
+
+public class testSyncManager extends TestCase{
+
+    public testSyncManager() {
+    }
+    
+    public void testGetInstance(){
+	SyncManager sm1 = SyncManager.getInstance();
+	SyncManager sm2 = SyncManager.getInstance();
+	assertEquals(sm1, sm2);
+    }
+
+}
