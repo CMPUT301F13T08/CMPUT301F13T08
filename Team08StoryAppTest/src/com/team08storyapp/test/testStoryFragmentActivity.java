1602b3c5c53647319556d9ea74ed92bdfd2e0302
301git <marcu@ualberta.ca>
2013-11-20 18:06:49 -0700

    make sure it's latest

--- Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragmentActivity.java
+++ Team08StoryAppTest/src/com/team08storyapp/test/testStoryFragmentActivity.java
@@ -10,6 +10,8 @@ import android.widget.LinearLayout;
 
 import com.team08storyapp.Choice;
 import com.team08storyapp.ChoiceAdapter;
+import com.team08storyapp.Photo;
+import com.team08storyapp.StoryFragment;
 import com.team08storyapp.StoryFragmentActivity;
 
 
@@ -19,9 +21,12 @@ ActivityInstrumentationTestCase2<StoryFragmentActivity>{
     private Activity activity;
     private ChoiceAdapter adapter;
     private ArrayList<Choice> Choices;
+    private ArrayList<Photo> Photos;
+    private StoryFragment storyFragment;
     private Choice choice1;
     private Choice choice2;
-    
+    private Photo photo1;
+    private Photo photo2;
     
     public testStoryFragmentActivity() {
   	super(StoryFragmentActivity.class);
@@ -29,11 +34,16 @@ ActivityInstrumentationTestCase2<StoryFragmentActivity>{
       
     public void setUp(){
 	
+	storyFragment = new StoryFragment(1);
+	storyFragment.setStoryText("Story Fragment Text");
+	
 	Choices = new ArrayList<Choice>();
 	choice1 = new Choice(2, 1, "Go to 2!");
 	choice2 = new Choice(3, 2, "Go to 3!");
 	Choices.add(choice1);
 	Choices.add(choice2);
+	
+	storyFragment.setChoices(Choices);
 	
 	activity = super.getActivity();
 
