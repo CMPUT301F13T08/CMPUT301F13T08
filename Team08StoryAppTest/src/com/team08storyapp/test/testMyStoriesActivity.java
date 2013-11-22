e5602b1cf51f7e86c8335973ecfddd4d23f152dd
301git <marcu@ualberta.ca>
2013-11-22 15:50:19 -0700

    list view test now fails in testMyStoriesActivity edit text test passes

--- Team08StoryAppTest/src/com/team08storyapp/test/testMyStoriesActivity.java
+++ Team08StoryAppTest/src/com/team08storyapp/test/testMyStoriesActivity.java
@@ -1,9 +1,11 @@
 package com.team08storyapp.test;
 
 import android.app.Activity;
+import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.team08storyapp.ESHelper;
@@ -22,9 +24,11 @@ public class testMyStoriesActivity extends
     private ESHelper esHelper;
     private FileHelper fHelper;
     private ListView listView;
+    private EditText editText;
     private int containLosSantos = 0;
     private Story story;
     private String title;
+    
 
     public testMyStoriesActivity() {
 	super(MyStoriesActivity.class);
@@ -33,7 +37,7 @@ public class testMyStoriesActivity extends
     public void setUp() {
 
 	activity = getActivity();
-
+	
 	fHelper = new FileHelper(activity, 1);
 	esHelper = new ESHelper();
 
@@ -41,7 +45,7 @@ public class testMyStoriesActivity extends
 		null);
 	searchButton = (Button) header.findViewById(R.id.searchButton);
 	listView = (ListView) activity.findViewById(android.R.id.list);
-
+	editText = (EditText) header.findViewById(R.id.searchText);
     }
 
     public void testPreConditions() {
@@ -49,22 +53,24 @@ public class testMyStoriesActivity extends
 	assertNotNull(header);
 	assertNotNull(searchButton);
 	assertNotNull(listView);
+	assertNotNull(editText);
 
     }
 
-    public void testHeaderText() {
+    public void testEditViewItem() {
 
-	assertEquals("", header.getContext().toString());
+	assertEquals("", editText.getText().toString());
 
     }
 
+    // not sure why fails
     public void testListViewItem() {
 	
+	// NULL POINTER EXCEPTION if start with i=0
 	for (int i = 1; i == listView.getCount(); i++) {
 
 	    story = (Story) listView.getItemAtPosition(i);
 	    title = story.getTitle().toString();
-	    //title = ((Story) listView.getItemAtPosition(i)).getTitle().toString();
 	   
 	    if (title == "Los Santos"){
 		containLosSantos = 1;
