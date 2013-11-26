b0b1230667c1c89f4c0084f4c614fa212d56d257
AliceM <mingxun@ualberta.ca>
2013-11-25 22:16:00 -0700

    remove unused imports in MyStoriesActivity

--- Team08StoryApp/src/com/team08storyapp/MyStoriesActivity.java
+++ Team08StoryApp/src/com/team08storyapp/MyStoriesActivity.java
@@ -45,7 +45,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
