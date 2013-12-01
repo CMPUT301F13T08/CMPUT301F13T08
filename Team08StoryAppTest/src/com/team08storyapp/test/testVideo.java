18b9146274b53a203f36ee51b754f0184af08dda
Jiawei Shen <jishent>
2013-11-30 23:17:46 -0700

    for fake test

--- Team08StoryAppTest/src/com/team08storyapp/test/testVideo.java
+++ Team08StoryAppTest/src/com/team08storyapp/test/testVideo.java
@@ -1,33 +1,3 @@
-/*
-AUTHORS
-========
-Alice Wu, Ana Marcu, Michele Paulichuk, Jarrett Toll, Jiawei Shen.
-
-LICENSE
-=======
-Copyright  ���  2013 Alice Wu, Ana Marcu, Michele Paulichuk, Jarrett Toll, Jiawei Shen,  
-Free Software Foundation, Inc., Marky Mark  License GPLv3+: GNU
-GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
-This program is free software: you can redistribute it and/or modify it under the terms of 
-the GNU General Public License as published by the Free Software Foundation, either 
-version 3 of the License, or (at your option) any later version. This program is distributed 
-in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public 
-License for more details. You should have received a copy of the GNU General Public License 
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-              
-3rd Party Libraries
-=============
-Retrieved Oct. 27, 2013 - https://github.com/rayzhangcl/ESDemo
--This demo was used to help with JSON and ESHelper which is under the CC0 licenses
-
-Retrieved Oct. 29, 2013  - http://hc.apache.org/downloads.cgi
--This is for the fluent library which is licensed under apache V2
-
-Retrieved Oct. 29, 2013 
-- https://code.google.com/p/google-gson/downloads/detail?name=google-gson-2.2.4-release.zip&can=2&q=
--This is for JSON which is licensed under apache V2
- */
 package com.team08storyapp.test;
 
 import static org.junit.Assert.*;
@@ -36,6 +6,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.team08storyapp.Video;
 import com.team08storyapp.MainActivity;
 
 @Ignore
@@ -64,7 +35,7 @@ public class testVideo extends ActivityInstrumentationTestCase2<MainActivity>{
         video.setVideoPermission(1);
         
         //Fails
-        asserEquals(1, video.getVideoID());
+        assertEquals(1, video.getVideoID());
         assertEquals("audio", video.getVideoName());
         assertEquals("encoded", video.getEncodedVideo());
         assertEquals(1, video.getVideoPermission());
